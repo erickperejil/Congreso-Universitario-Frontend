46a797661ac5019590fea1dfa04920b762fbe4ae
erickperejil <erickdanielescoto@gmail.com>
2024-12-20 17:42:03 -0600

    Estructura de NextJS

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Cronograma from "@/components/cronograma";
+
+export default function Home() {
+  return (
+  <div>
+    <Cronograma></Cronograma>
+  </div>
+  );
+}
---
+++ src/components/cronograma.tsx
@@ -0,0 +1,8 @@
+export default function Cronograma() {
+    return (
+    <div>
+      trabar aqui
+    </div>
+    );
+  }
+  
