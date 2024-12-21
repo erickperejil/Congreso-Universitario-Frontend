1fba2d7051ba5383e066405c9880055aed4452fb
samuenoc <sreyesz@unah.hn>
2024-12-21 16:02:06 -0600

    archivos json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Cronograma from "@/components/cronograma";
-import Ponentes from "@/components/ponentes";
 
 export default function Home() {
   return (
   <div>
     <Cronograma></Cronograma>
-    <Ponentes></Ponentes>
   </div>
   );
 }
