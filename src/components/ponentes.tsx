2348627ba6ace8bb156418cdbfd69120061a19e1
samuenoc <sreyesz@unah.hn>
2024-12-21 17:30:07 -0600

    fix en responsive ponentes

--- src/components/ponentes.tsx
+++ src/components/ponentes.tsx
@@ -63,7 +63,7 @@ export default function Ponentes() {
       <h1 className="text-[#E7E8F2] text-3xl font-bold text-center mb-6">
         Ponentes
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-12">
         {ponentes.map((ponente, index) => (
           <div
             key={index}
