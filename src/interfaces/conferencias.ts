bee7f41d7345094ba84cb0c49c2a520d930e2033
samuenoc <sreyesz@unah.hn>
2024-12-30 21:03:12 -0600

    interfaz de conferencia

--- src/interfaces/conferencias.ts
+++ src/interfaces/conferencias.ts
@@ -11,6 +11,7 @@ export interface CreateConferencia {
     hora_final: string; // Formato completo de fecha y hora "YYYY-MM-DD HH:mm"
     cupos: string; // Puede mantenerse como string o cambiarse a número según cómo lo manejes
     img_conferecia: string[] | null;
+    
   }
   export interface Conferencia {
     nombre_ponente: string;
