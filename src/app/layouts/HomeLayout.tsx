482f7a453a5af5aa3d202c15937b5760244e28b5
erickperejil <erickdanielescoto@gmail.com>
2025-01-03 17:38:00 -0600

    boton absolute

--- src/app/layouts/HomeLayout.tsx
+++ src/app/layouts/HomeLayout.tsx
@@ -43,7 +43,7 @@ export default function HomeLayout({ navOptions, children }: { navOptions: { nam
                 onClick={() => setSidebarOpen(!isSidebarOpen)}
                 aria-controls="logo-sidebar"
                 type="button"
-                className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                className="absolute inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             >
                 <span className="sr-only">Open sidebar</span>
                 <svg
