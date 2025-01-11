2dea48cc822d1d0e7c233cdf73bc6cd55b43975f
KennethJContreas <kennethcontreras2017@gmail.com>
2025-01-11 09:09:50 -0600

    Corrigiendo middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-/* import jwt from 'jsonwebtoken';
- */
+import jwt from 'jsonwebtoken';
+
 export function middleware(request: NextRequest) {
-     console.log('Middleware:', request.nextUrl.pathname);
-/*     const token = request.cookies.get('authToken')?.value;
+    console.log('Middleware:', request.nextUrl.pathname);
+ /*    const token = request.cookies.get('authToken')?.value;
 
     // Define las rutas públicas (que no requieren autenticación)
     const publicRoutes = ['/login', '/register'];
@@ -31,26 +31,46 @@ export function middleware(request: NextRequest) {
         }
 
         // Redirigir según el rol
-        if (decoded.tipo_usuario === 'administrador' || decoded.tipo_usuario === 'organizador') {
-            // Administradores y organizadores solo pueden acceder a /admin
-            if (!request.nextUrl.pathname.startsWith('/admin')) {
-                return NextResponse.redirect(new URL('/admin/home', request.url));
-            }
-        } else if (decoded.tipo_usuario === 'comun') {
-            // Los usuarios solo pueden acceder a /my
-            if (!request.nextUrl.pathname.startsWith('/my')) {
-                return NextResponse.redirect(new URL('/my', request.url));
-            }
+        switch (decoded.tipo_usuario) {
+            case 'administrador':
+                // Administradores pueden acceder a /admin
+                if (!request.nextUrl.pathname.startsWith('/admin')) {
+                    return NextResponse.redirect(new URL('/admin/home', request.url));
+                }
+                break;
+            case 'organizador':
+                // Organizadores pueden acceder a /admin
+                if (!request.nextUrl.pathname.startsWith('/organizador')) {
+                    return NextResponse.redirect(new URL('/organizador', request.url));
+                }
+                break;
+            case 'comun':
+                // Usuarios pueden acceder a /my
+                if (!request.nextUrl.pathname.startsWith('/my')) {
+                    return NextResponse.redirect(new URL('/my', request.url));
+                }
+                break;
+            case 'colaborador':
+                // Colaboradores pueden acceder a /my
+                if (!request.nextUrl.pathname.startsWith('/colaborador')) {
+                    return NextResponse.redirect(new URL('/colaborador', request.url));
+                }
+                break;
+            default:
+                // Si el rol no es válido, redirige al login
+                return NextResponse.redirect(new URL('/login', request.url));
         }
+
+
     } catch (error) {
         // Si la decodificación falla, redirige al login
         console.error('Invalid token:', error);
         return NextResponse.redirect(new URL('/login', request.url));
-    }
- */
+    } */
+
     // Si el token es válido y no ha expirado, permite continuar
     return NextResponse.next();
- }
+}
 
 // Configura el middleware para aplicarlo a todas las rutas
 export const config = {
@@ -60,3 +80,14 @@ export const config = {
 };
 
 
+        /*         if (decoded.tipo_usuario === 'administrador' || decoded.tipo_usuario === 'organizador') {
+                    // Administradores y organizadores solo pueden acceder a /admin
+                    if (!request.nextUrl.pathname.startsWith('/admin')) {
+                        return NextResponse.redirect(new URL('/admin/home', request.url));
+                    }
+                } else if (decoded.tipo_usuario === 'comun') {
+                    // Los usuarios solo pueden acceder a /my
+                    if (!request.nextUrl.pathname.startsWith('/my')) {
+                        return NextResponse.redirect(new URL('/my', request.url));
+                    }
+                } else if */
